c703fc417020f8d8e1541ee301465cce88b94225
Joshua Curtis <joshua.mi.curtis@gmail.com>
2017-03-21 18:30:05 -0400

    Testing adding a second to do item

--- functional_tests.py
+++ functional_tests.py
@@ -38,10 +38,26 @@ class NewVisitorTest(unittest.TestCase):
         
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feather', [row.text for row in rows])
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_list_table')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        # The page updates again, and shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(
+            '1: Buy peacock feathers', 
+            [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly', 
+            [row.text for row in rows]
+        )
+        
         self.fail('Finish the test!')
         # Site generates a unique URL for this list and has text telling
         # the user that her list is saved
